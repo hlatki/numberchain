0213476ef3718f27f96dc8b5ebd8b3877f07e16a
hlatki <hannahatkinson@gmail.com>
2014-09-27 00:34:47 -0700

    Generate and set number list and target sum

--- src/number_chain/core.cljs
+++ src/number_chain/core.cljs
@@ -1,7 +1,12 @@
 (ns number-chain.core
   (:require [reagent.core :as reagent :refer [atom]]))
 
-(def app-state (atom {:app-name "Number Chain"}))
+(def NUM_SQUARES 24)        ;; Number of squares
+(def MAX_NUM_SUMMANDS 5)    ;; Maximum number of summands (must be less than
+                            ;; NUM_SQUARES)
+(def MAX_SUMMAND 10)        ;; Maximum value of any summand
+
+(def app-state (atom {:app-name "Number Chain" :numbers "" :target ""}))
 
 (defn get-element-by-id [id]
   (.getElementById js/document id))
@@ -16,3 +21,26 @@
 
 (comment
   (js/alert "Hello Justin!"))
+
+
+;; The following functions generate the numbers to populate the grid and then
+;; set the appropriate atom
+(defn generate-numbers
+  "Helper function that generates a list containing NUM_SQUARES integers"
+  []
+  (take NUM_SQUARES (repeatedly #(inc (rand-int MAX_SUMMAND)))))
+
+(defn generate-target
+  "Helper function that figures out what the target should be"
+  [list-of-nums]
+  (let [num-summands (inc (rand-int MAX_NUM_SUMMANDS))]
+    (apply + (take num-summands list-of-nums))))
+
+(defn set-numbers-and-target!
+  "Set the state of the numbers and the sum target -- call this one"
+  []
+  (let [num-list (generate-numbers)
+        target   (generate-target num-list)]
+    (swap! app-state assoc :numbers (shuffle num-list) :target target)))
+
+
